5563c2b86255ee6bee2926f8934af39cc2437c3b
Jan Broer <janeczku@yahoo.de>
2015-08-04 20:35:53 +0200

    Fix access to Dropbox share

--- container-files/config/init/12-calibre-path.sh
+++ container-files/config/init/12-calibre-path.sh
@@ -6,8 +6,10 @@
 sed -i "s,^DB_ROOT.*,DB_ROOT = $CALIBRE_PATH," /opt/app/config.ini
 echo "Calibre DB folder set to $CALIBRE_PATH"
 
-#if [ -f "$CALIBRE_PATH/metadata.db" ]
-#then
-#        chmod 777 ${CALIBRE_PATH}/metadata.db
-#        echo "Made calibre metadata.db writable"
-#fi
+if [ -f "$CALIBRE_PATH/metadata.db" ]
+then
+		f=$CALIBRE_PATH
+		while [[ $f != "/" ]]; do chmod +ox $f; f=$(dirname $f); done;
+        chmod 777 ${CALIBRE_PATH}/metadata.db
+        echo "Made calibre metadata.db writable"
+fi
