8ea64a55f14c3645f0dbe48443b4b6e94170cbcb
Jan Broer <janeczku@yahoo.de>
2015-08-04 22:29:41 +0200

    Better method to change calibre folder permissions

--- container-files/config/init/12-calibre-path.sh
+++ container-files/config/init/12-calibre-path.sh
@@ -10,7 +10,10 @@ if [ -f "$CALIBRE_PATH/metadata.db" ]
 then
 		f=$CALIBRE_PATH
 		while [[ $f != "/" ]]; do chmod o+x $f; f=$(dirname $f); done;
-		chmod -R o+rx ${CALIBRE_PATH}
+		#chmod -R o+rx ${CALIBRE_PATH}
+		find ${CALIBRE_PATH} -type d -exec chmod o+rx {} +
         chmod 777 ${CALIBRE_PATH}/metadata.db
         echo "Made calibre metadata.db writable"
 fi
+
+
