7aa2c4833cd226271774c71b9b3282eeabfbb148
Jan Broer <janeczku@yahoo.de>
2016-03-26 17:39:03 +0100

    Fix bash init script error

--- container-files/config/init/14-ssl-certs.sh
+++ container-files/config/init/14-ssl-certs.sh
@@ -4,10 +4,12 @@
 #
 
 DEFAULT_HOST_CONF="/etc/nginx/hosts.d/default.conf"
+CERT=${SSL_CERT_NAME:-}
+KEY=${SSL_KEY_NAME:-}
 
-if [ -n "${SSL_CERT_NAME}" ] && [ -n "${SSL_KEY_NAME}" ]; then
-	sed -i "s,ssl_certificate\s.*,ssl_certificate /etc/nginx/ssl/$SSL_CERT_NAME;," DEFAULT_HOST_CONF
-	sed -i "s,ssl_certificate_key\s.*,ssl_certificate_key /etc/nginx/ssl/$SSL_KEY_NAME;," DEFAULT_HOST_CONF
-	echo "Set SSL certificate to $SSL_CERT_NAME"
-	echo "Set SSL private key to $SSL_KEY_NAME"
+if [ ! -z ${CERT} ] && [ ! -z ${KEY} ]; then
+	sed -i "s,ssl_certificate\s.*,ssl_certificate /etc/nginx/ssl/$CERT;," DEFAULT_HOST_CONF
+	sed -i "s,ssl_certificate_key\s.*,ssl_certificate_key /etc/nginx/ssl/$KEY;," DEFAULT_HOST_CONF
+	echo "Set SSL certificate to $CERT"
+	echo "Set SSL private key to $KEY"
 fi
